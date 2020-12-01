0e0fb2fec7347733d2d8e898a3c6d037341430fd
Geralt555 <longer2@yandex.ru>
2020-12-01 12:06:58 +0300

    ind1.py

--- ind1.py
+++ ind1.py
@@ -1,30 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #  Дано число m(от 1 до 12 ). Определить полугодие, на которое приходится месяц
 #  c номером и количество дней в том месяце (год не високосный).
 
-y = int(input('Введите номер месяца'))
-if y == 1:
-    print('Январь ')
-elif y == 2:
-    print('Февраль')
-elif y == 3:
-    print('Март')
-elif y == 4:
-    print('Апрель')
-elif y == 5:
-    print('Май')
-elif y == 6:
-    print('Июнь')
-elif y == 7:
-    print('Июль')
-elif y == 8:
-    print('Август')
-elif y == 9:
-    print('Сентябрь')
-elif y == 10:
-    print('Октябрь')
-elif y == 11:
-    print('Ноябрь')
-elif y == 12:
-    print('Декабрь')
-else:
-    print('x <')
+if __name__ == '__main__':
+    y = int(input('Введите номер месяца'))
+    if y == 1:
+        print('Январь ')
+    elif y == 2:
+        print('Февраль')
+    elif y == 3:
+        print('Март')
+    elif y == 4:
+        print('Апрель')
+    elif y == 5:
+        print('Май')
+    elif y == 6:
+        print('Июнь')
+    elif y == 7:
+        print('Июль')
+    elif y == 8:
+        print('Август')
+    elif y == 9:
+        print('Сентябрь')
+    elif y == 10:
+        print('Октябрь')
+    elif y == 11:
+        print('Ноябрь')
+    elif y == 12:
+        print('Декабрь')
+    else:
+        print('x <')
