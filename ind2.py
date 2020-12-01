b521f297d9786a345be177f8c44f6c33571335ba
Geralt555 <longer2@yandex.ru>
2020-12-01 12:07:24 +0300

    ind2.py

--- ind2.py
+++ ind2.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 #  Решить неравенство  , где a - произвольное действительное число.
 
-a = float(input())
-tmp = '= ' + str(a)
-if a >= 7 / 4:
-    tmp = ' ' + str(min(a, 3 / 2 + (a - 7 / 4) ** 0.5))
-print('x <' + tmp)
+if __name__ == '__main__':
+    a = float(input())
+    tmp = '= ' + str(a)
+    if a >= 7 / 4:
+        tmp = ' ' + str(min(a, 3 / 2 + (a - 7 / 4) ** 0.5))
+    print('x <' + tmp)
